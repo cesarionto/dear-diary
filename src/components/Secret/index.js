8c9f70e909b74117e3cff3757b740da0c517d2cd
cesarionto <cesariopereiraneto@gmail.com>
2020-07-16 14:02:58 -0300

    formatação da data

--- src/components/Secret/index.js
+++ src/components/Secret/index.js
@@ -3,13 +3,26 @@ import './style.css'
 import FormUpdate from '../FormUpdate'
 
 function Secret({id, subject, content, publicationDate}) {
-    const [visibility, setVisibility] = useState(false)  
+    const [visibility, setVisibility] = useState(false)
+    const dateFormat = () => {
+        const data = new Date()
+        let day = data.getDate()
+        let month = data.getMonth() + 1
+        let year = data.getFullYear()
+
+        if (day.toString().length === 1)
+            day = '0' + day
+        if (month.toString().length === 1)
+            month = '0' + month
+
+        return day + '/' + month + '/' + year
+    }  
     return (
         <div>
             <div className="secret">
                 <div>
                     <h3>{subject}</h3>
-                    <h6>Data de Publicação: {publicationDate}</h6>  
+                    <h6>Data de Publicação: {dateFormat(publicationDate)}</h6>  
                     <p>{content}</p>  
                 </div>
                 <div>
